e49d07a64ea0eb4f5a199c2bf9ea4c8aee2b313f
Tonkers Kuma <tonkers.society@gmail.com>
2021-10-29 14:10:39 -0700

    claim script

--- scripts/claim.py
+++ scripts/claim.py
@@ -5,15 +5,15 @@ import json
 
 
 def main():
-    # dev = accounts.load(click.prompt("Account", type=click.Choice(accounts.load())))
+    dev = accounts.load(click.prompt("Account", type=click.Choice(accounts.load())))
     merkleOrchard = Contract("0xdAE7e32ADc5d490a43cCba1f0c736033F2b4eFca")
     bal = "0xba100000625a3754423978a60c9317c58a424e3D"
     ldo = "0x5A98FcBEA516Cf06857215779Fd812CA3beF1B32"
     bal_distributor = "0xd2EB7Bd802A7CA68d9AcD209bEc4E664A9abDD7b"
     # ldo_distributor = "" use legacy claim. Orchard not set up yet for ldo
-    rewards = [("homestead.json", bal, bal_distributor, "BAL"),
-               # ("homestead-lido.json", ldo, ldo_distributor, "LDO")
-               ]
+    rewards = [("homestead.json", bal, bal_distributor, "BAL")]
+    # ("homestead-lido.json", ldo, ldo_distributor, "LDO")
+
     for reward in rewards:
         f = open(f'./scripts/{reward[0]}', )
         data = json.load(f)
@@ -27,8 +27,5 @@ def main():
                       reward[2],
                       0,
                       token_data["hex_proof"])]
-            merkleOrchard.claimDistributions(token_data["address"],
-                                             claim,
-                                             [reward[1]],
-                                             {'from': dev, 'priority_fee': '1 gwei', 'max_fee': '130 gwei'})
+            merkleOrchard.claimDistributions(token_data["address"], claim, [reward[1]], {'from': dev})
             print(f'{token_data["address"]} claimed {int(token_data["claim_amount"]) / 1e18} {reward[3]} ')
