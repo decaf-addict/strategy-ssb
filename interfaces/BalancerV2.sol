5478241190dea4c062a81a30042e92b7cec6bf59
Tonkers <tonkers.society@gmail.com>
2022-01-05 16:21:27 -0800

    new interfaces

--- interfaces/BalancerV2.sol
+++ interfaces/BalancerV2.sol
@@ -29,7 +29,7 @@ interface IBalancerPool is IERC20 {
         uint256[] memory balances,
         uint256 indexIn,
         uint256 indexOut
-    ) external returns (uint256 amount);
+    ) external view returns (uint256 amount);
 }
 
 interface IBalancerVault {
@@ -92,8 +92,8 @@ interface IBalancerVault {
     struct SingleSwap {
         bytes32 poolId;
         SwapKind kind;
-        IAsset assetIn;
-        IAsset assetOut;
+        address assetIn;
+        address assetOut;
         uint256 amount;
         bytes userData;
     }
@@ -199,13 +199,12 @@ interface IBalancerVault {
         uint256 deadline
     ) external payable returns (int256[] memory);
 
-    // CAVEAT!! Do not call this after a batchSwap in the same txn
-    function queryBatchSwap(
-        SwapKind kind,
-        BatchSwapStep[] memory swaps,
-        IAsset[] memory assets,
-        FundManagement memory funds
-    ) external returns (int256[] memory);
+    function flashLoan(
+        address recipient,
+        IERC20[] memory tokens,
+        uint256[] memory amounts,
+        bytes memory userData
+    ) external;
 }
 
 interface IAsset {
@@ -240,4 +239,58 @@ interface IBalancerVaultHelper {
         address recipient,
         IBalancerVault.ExitPoolRequest memory request
     ) external view returns (uint256 bptIn, uint256[] memory amountsOut);
+}
+
+interface IFlashLoanRecipient {
+    /**
+     * @dev When `flashLoan` is called on the Vault, it invokes the `receiveFlashLoan` hook on the recipient.
+     *
+     * At the time of the call, the Vault will have transferred `amounts` for `tokens` to the recipient. Before this
+     * call returns, the recipient must have transferred `amounts` plus `feeAmounts` for each token back to the
+     * Vault, or else the entire flash loan will revert.
+     *
+     * `userData` is the same value passed in the `IVault.flashLoan` call.
+     */
+    function receiveFlashLoan(
+        IERC20[] memory tokens,
+        uint256[] memory amounts,
+        uint256[] memory feeAmounts,
+        bytes memory userData
+    ) external;
+}
+
+interface ILinearPool {
+    function getVault() external view returns (address);
+    
+    function getPoolId() external view returns (bytes32 poolId);
+
+    function getMainToken() external view returns (address);
+
+    function getWrappedToken() external view returns (address);
+
+    function getBptIndex() external view returns (uint256);
+
+    function getMainIndex() external view returns (uint256);
+
+    function getWrappedIndex() external view returns (uint256);
+
+    // Price rates
+
+    /**
+     * @dev For a Linear Pool, the rate represents the appreciation of BPT with respect to the underlying tokens. This
+     * rate increases slowly as the wrapped token appreciates in value.
+     */
+    function getRate() external view returns (uint256);
+
+    function getWrappedTokenRate() external view returns (uint256);
+
+    function getTargets() external view returns (uint256 lowerTarget, uint256 upperTarget);
+
+    /**
+     * @dev Returns the number of tokens in circulation.
+     *
+     * In other pools, this would be the same as `totalSupply`, but since this pool pre-mints all BPT, `totalSupply`
+     * remains constant, whereas `virtualSupply` increases as users join the pool and decreases as they exit it.
+     */
+    function getVirtualSupply() external view returns (uint256);
 }
